ae779495db138431354809b1467f3d8846d273b3
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-04-26 19:01:47 +0700

    Add files via upload

---
+++ StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Удаление слова wordsToRemove из строки input
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="wordsToRemove"></param>
+        /// <returns></returns>
+        public static string RemoveWords(string input, string[] wordsToRemove)
+        {
+            string pattern = @"\b(" + string.Join("|", wordsToRemove.Select(Regex.Escape)) + @")\b";
+            return Regex.Replace(input, pattern, "", RegexOptions.IgnoreCase);
+        }
+
+        /// <summary>
+        /// Удаление слова wordsToRemove и лишних пробелов из строки input 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="wordsToRemove"></param>
+        /// <returns></returns>
+        public static string RemoveWordsClean(string input, string[] wordsToRemove)
+        {
+            string pattern = @"\s*\b(" + string.Join("|", wordsToRemove.Select(Regex.Escape)) + @")\b\s*";
+            string result = Regex.Replace(input, pattern, " ", RegexOptions.IgnoreCase);
+            return Regex.Replace(result, @"\s+", " ").Trim();
+        }
+    }
+}
